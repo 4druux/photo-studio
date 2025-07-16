9d7cc629ca07dc06c45663c58803c653fc32d72e
andrewsamagachandra <andrewsamagachandra@gmail.com>
2025-07-16 07:47:12 +0700

    remove bolt

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
   title: "Antika Studio",
   description:
     "Antika Studio adalah studio fotografi profesional yang menyediakan layanan foto pernikahan, potret keluarga, komersial, dan produk. Dilengkapi fasilitas studio modern dan tim fotografer berpengalaman, kami membantu Anda mengabadikan momen berharga dengan kualitas terbaik.",
+  icons: {
+    icon: "/images/logo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
